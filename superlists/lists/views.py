ca37c14d822caae174c7e70cba97fd8a36c83eff
Krzysztof Żuraw <krzysztof.zuraw@stxnext.pl>
2017-04-27 08:56:15 +0200

    Before refactor

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    # if request.method == 'POST':
+    return render(
+        request, 'home.html', {
+            'new_item_text': request.POST.get('item_text', ''),
+        }
+    )
+    # return render(request, 'home.html')
