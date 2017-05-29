b2a074c3c49382314d54368fcd551e4725dc7b67
Krzysztof Żuraw <krzysztof.zuraw@stxnext.pl>
2017-05-29 10:17:19 +0200

    Failing test for multiple users

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -67,13 +67,13 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith wonders whether the site will remember her list. Then
         # she sees that the site has generated a unique URL for her -- there
         # is some explanatory text to that effect.
-        self.fail('Finish the test!')
+        # self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
-        # Satified, she goes back to sleep
+        # Satisfied, she goes back to sleep
 
-    def hide_test_multiple_users_can_start_at_different_urls(self):
+    def test_multiple_users_can_start_at_different_urls(self):
         # Edith start a new todo list
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -98,3 +98,22 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith
+        inputbox = self.browser.find_element_by_id('item_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis gets his own URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, both goes back to sleep
